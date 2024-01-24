b70bf907eabc26584f351de6e356295e8b67c1b0
Supernoodles99 <155268413+Supernoodles99@users.noreply.github.com>
2024-01-24 13:07:15 -0600

    Create cooleffect.js
    from this tutorial by hyperplexed: https://www.youtube.com/watch?v=W5oawMJaXbU

---
+++ cooleffect.js
@@ -0,0 +1,18 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+document.querySelector("h1").onmouseover = event => {
+let iterations = 0;
+  
+ const interval = setInterval(() => { 
+    event.target.innerText = event.target.innerText.split("")
+      .map((letter, index) => {
+      if (index < iterations) {
+        return event.target.dataset.value[index];
+      }
+     return letters[Math.floor(Math.random() * 36)] 
+    })
+      .join("")
+   if (iterations >= event.target.dataset.value.length) {clearInterval(interval)}; 
+   
+   iterations+= 1 / 2;
+  }, 30)
+}
